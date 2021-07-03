254378c95059dd96d45d36dc72194f2eaa59701f
Aldyn306429 <65444770+Aldyn306429@users.noreply.github.com>
2021-07-03 00:15:53 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println(" __         __");
+    System.out.println("/  \\.-\"\"\"-./  \\");
+    System.out.println("\\    -   -    /");
+    System.out.println(" |   o   o   |");
+    System.out.println(" \\  .-'''-.  /");
+    System.out.println("  \'-\\__Y__/-\'");
+    System.out.println("     `---`");
+  }
+}
